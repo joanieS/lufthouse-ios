cc0dac2094570000f4b01bfc4434c48b0a7d7a67
Adam Gleichsner <amg188@case.edu>
2014-08-20 20:21:42 -0400

    Unimportant commits to catch up develop branch before restoring a stash.

---
+++ lufthouse-ios/SupportViewController.h
@@ -0,0 +1,14 @@
+//
+//  SupportViewController.h
+//  lufthouse-ios
+//
+//  Created by Adam Gleichsner on 8/1/14.
+//  Copyright (c) 2014 Lufthouse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SupportViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIWebView *webView;
+
+@end
